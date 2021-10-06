99014eace4fd7043aa13e3b6003c28a3737c89fc
benachourghailen@gmail.com <benachourghailen@gmail.com>
2021-10-06 08:17:14 +0100

    Class definition

--- include/algorithms.hpp
+++ include/algorithms.hpp
@@ -10,4 +10,16 @@ public:
     void Show() const;
     float GetEpsilon() const;
     void SetEpsilon(float e);
+private:
+const int _stateSize;
+const int _actionSize;
+const int _range;
+std::map<std::vector<int>, std::vector<float>> _q_table;
+bool _init;
+float _EPSILON;
+static double _DISCOUNT;
+const static int _EPISODES = 10;
+static float _EPS_DECAY;
+static double _LEARNING_RATE;
+
 };
